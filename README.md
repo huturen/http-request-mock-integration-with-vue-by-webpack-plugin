286df9e48669b679f771931d3d644a984fc57dc6
hushuilong <shuilonghu@tencent.com>
2022-03-03 15:59:52 +0800

    chore: first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# http-request-mock-with-webpack-plugin
+
+## Project setup
+```
+npm install
+```
+
+## Mock development
+```
+npm run mock-dev
+```
+
+### Unit Test
+```
+npm run test
+```
---
+++ test/demo.test.js
@@ -0,0 +1,16 @@
+/* global it, expect */
+import axios from 'axios';
+import xhrAdapter from 'axios/lib/adapters/xhr';
+import HttpRequestMock from 'http-request-mock';
+
+axios.defaults.adapter = xhrAdapter;
+const mocker = HttpRequestMock.setupForUnitTest('xhr');
+
+mocker.get('https://your.api.com/path', function() {
+  return { abc: 123 };
+});
+
+it('should match object`', async () => {
+  const res = await axios.get('https://your.api.com/path');
+  expect(res.data).toMatchObject({abc: 123});
+});
